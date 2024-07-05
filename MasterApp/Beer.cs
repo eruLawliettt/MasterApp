54fcc0716c8c68d4c326265d7f8756946a92b933
Raisel <jidayoo76@gmail.com>
2024-07-05 20:41:04 +0300

    add beer

---
+++ MasterApp/Beer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterApp
+{
+    public class Beer
+    {
+        private int Id {  get; init; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        
+
+        public Beer(int id,string title,decimal price)
+        {
+           Id = id;
+           Title = title;
+           Price = price;
+        }
+        public override string ToString()
+        {
+            return $"ЕЕ я Дима Негодненко и я купил пиво {Title} за {Price } рублей";
+        }
+    }
+  
+}
--- MasterApp/Program.cs
+++ MasterApp/Program.cs
@@ -4,3 +4,6 @@ Console.WriteLine("Hello, World!");
 
 Person person = new(1, "Russel");
 Console.WriteLine("Я " + person.ToString());
+Console.WriteLine( );
+Beer beer = new(1, "Клянское", 120);
+Console.WriteLine(beer.ToString());
